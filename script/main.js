309c3342a3b89f4e53829ca32eb16dde1c58eb98
postfm <postfm@yandex.ru>
2020-12-05 17:09:04 +0300

    Finished layoutpage

---
+++ script/main.js
@@ -0,0 +1,24 @@
+$(function () {
+  $(".fairy-tail__slider, .our-trip__slider").slick({
+    fade: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+  $(".header__menu-list").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
